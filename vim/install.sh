991c5d8f1b55820bf362421f90858944a8a0c6f6
Ryoko Tominaga <ryokotmng@gmail.com>
2020-05-02 17:04:15 +0900

    fix installer file for vim

---
+++ vim/install.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+DOT_DIRECTORY="${HOME}/dotfiles"
+DOT_CONFIG_DIRECTORY="${HOME}/.config"
+
+if [[ ! -e $DOT_CONFIG_DIRECTORY ]]; then
+  mkdir -p $DOT_CONFIG_DIRECTORY
+fi
+
+ln -snfv $DOT_DIRECTORY/.zprofile $HOME/.zprofile
+ln -snfv $DOT_DIRECTORY/.zshrc $HOME/.zshrc
+ln -snfv $DOT_DIRECTORY/vim/vimrc $DOT_CONFIG_DIRECTORY/nvim/init.vim
+ln -snfv $DOT_DIRECTORY/vim/vimrc $HOME/.vimrc
+ln -snfv $DOT_DIRECTORY/vim/.ideavimrc $HOME/.ideavimrc
+
+echo "linked .zshprofile and zshrc complete!"
