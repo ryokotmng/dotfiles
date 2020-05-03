7629eb897601631ea351c6ce468888fc2797f9a6
Ryoko Tominaga <ryokotmng@gmail.com>
2020-05-04 07:42:22 +0900

    fix tmux files

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,11 +1,15 @@
-#!/bin/zsh
-if [[ ! -e $HOME/.tmux.conf ]]; then
-  ln -s ~/tmux.conf ~/.tmux.conf
+#!/bin/bash
+if [[ ! -d $HOME/.tmux-themepack ]]; then
+    git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
+fi
 
-  case "$(uname)" in
-    "Linux" )
-      ln -s $PWD/tmux.conf.linux ~/.tmux.conf.linux;;
-    "Darwin")
-      ln -s $PWD/tmux.conf.mac ~/.tmux.conf.mac;;
-  esac
+if [[ ! -e $HOME/.tmux.conf ]]; then
+    ln -s $PWD/tmux.conf ~/.tmux.conf
+
+    case "$(uname)" in
+      "Linux" )
+        ln -s $PWD/tmux.conf.linux ~/.tmux.conf.linux;;
+      "Darwin" )
+        ln -s $PWD/tmux.conf.mac ~/.tmux.conf.mac;;
+    esac
 fi
