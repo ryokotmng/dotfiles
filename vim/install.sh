b70d8fa8c108ca5289fb34b9d9cd279710ba438b
Ryoko Tominaga <ryokotmng@gmail.com>
2020-07-24 12:47:43 +0900

    create .tigrc

--- vim/install.sh
+++ vim/install.sh
@@ -8,6 +8,7 @@ fi
 
 ln -snfv $DOT_DIRECTORY/.zprofile $HOME/.zprofile
 ln -snfv $DOT_DIRECTORY/.zshrc $HOME/.zshrc
+ln -snfv $DOT_DIRECTORY/tigrc $HOME/.tigrc
 ln -snfv $DOT_DIRECTORY/vim/vimrc $DOT_CONFIG_DIRECTORY/nvim/init.vim
 ln -snfv $DOT_DIRECTORY/vim/vimrc $HOME/.vimrc
 ln -snfv $DOT_DIRECTORY/vim/.ideavimrc $HOME/.ideavimrc
