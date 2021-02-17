85bfd742be4f1de578ca898b6128a20e4764525e
富永良子 <ryokotmng@gmail.com>
2021-02-17 23:38:18 +0900

    move ideavimrc file to new idea folder and add Easymotion

--- vim/install.sh
+++ vim/install.sh
@@ -11,6 +11,6 @@ ln -snfv $DOT_DIRECTORY/zshrc $HOME/.zshrc
 ln -snfv $DOT_DIRECTORY/tigrc $HOME/.tigrc
 ln -snfv $DOT_DIRECTORY/vim/vimrc $DOT_CONFIG_DIRECTORY/nvim/init.vim
 ln -snfv $DOT_DIRECTORY/vim/vimrc $HOME/.vimrc
-ln -snfv $DOT_DIRECTORY/vim/.ideavimrc $HOME/.ideavimrc
+ln -snfv $DOT_DIRECTORY/idea/ideavimrc $HOME/.ideavimrc
 
 echo "linked .zshprofile and zshrc complete!"
