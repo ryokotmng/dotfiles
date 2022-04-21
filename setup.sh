d215c64ef62c9d95acbcdd483693951efac654ac
turbofish <ryokotmng@gmail.com>
2022-04-21 19:09:32 +0900

    brew install asdf

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ brew install tree
 brew install fzf
 brew install pyenv && brew install pyenv-virtualenv
 brew install neovim
+brew install asdf
 git clone git@github.com:rupa/z.git ~/z
 sh ~/dotfiles/vim/install.sh
 
