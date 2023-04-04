9c317e31508c8fc7a2d4e10dcd28aafb3777c29a
ryokotmng <ryokotmng@E146773.local>
2023-04-04 13:08:52 +0900

    installation for coc.vim

--- setup.sh
+++ setup.sh
@@ -2,15 +2,18 @@
 
 echo "Setup start!"
 
+brew install pyenv && brew install pyenv-virtualenv
+brew install node
+
 brew install lazygit
 brew install tree
 brew install fzf
-brew install pyenv && brew install pyenv-virtualenv
 brew install neovim
 brew install asdf
 brew install ffmpeg
 brew install gh
 brew install direnv
+
 git clone git@github.com:rupa/z.git ~/z
 sh ~/dotfiles/vim/install.sh
 
